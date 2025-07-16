568e1413481356ff3e91214e3542d9d5e48d4330
Gino Morichetti <ginomorichetti@gmail.com>
2025-07-16 13:35:28 -0300

    Feature - Screens, Navs links with styles done

---
+++ frontend/src/features/pomodoro/PomodoroPage.tsx
@@ -0,0 +1,11 @@
+import CountdownTimer from "./components/CountdownTimer/CountdownTimer"
+
+const PomodoroPage = () => {
+  return (
+    <div>
+      <CountdownTimer />
+    </div>
+  )
+}
+
+export default PomodoroPage
--- frontend/src/shared/design-system/Navbar/Navbar.tsx
+++ frontend/src/shared/design-system/Navbar/Navbar.tsx
@@ -1,16 +1,42 @@
-import styles from './Navbar.module.css'
-// import { useState } from "react"
+import { useState } from 'react';
+import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  // const [isHidden, setIsHidden] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
+  let hoverTimeout: ReturnType<typeof setTimeout>;
+
+  const showNavbar = () => {
+    clearTimeout(hoverTimeout);
+    setIsVisible(true);
+  };
+
+  const hideNavbar = () => {
+    hoverTimeout = setTimeout(() => setIsVisible(false), 100);
+  };
 
   return (
-    <div className={styles.navbarContainer}>
-      <h3>Time Tracker</h3>
+    <>
+      <div
+        className={styles.hoverZone}
+        onMouseEnter={showNavbar}
+        onMouseLeave={hideNavbar}
+      ></div>
 
-    </div>
-  )
-}
+      <div
+        className={`${styles.navbarContainer} ${isVisible ? styles.show : ''}`}
+        onMouseEnter={showNavbar}
+        onMouseLeave={hideNavbar}
+      >
 
-export default Navbar
+      <Link to='/' className={styles.navItem} >Pomodoro</Link>
+      <Link to='/projects' className={styles.navItem} >Projects</Link>
+      <Link to='/history' className={styles.navItem} >History</Link>
+
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
